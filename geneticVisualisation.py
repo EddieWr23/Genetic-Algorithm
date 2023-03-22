70a088f06d2611a98a4d495000fb317acf72e944
Eddie Wragg <eddiewragg@gmail.com>
2023-03-22 20:14:38 +0000

    commit

--- geneticVisualisation.py
+++ geneticVisualisation.py
@@ -2,17 +2,20 @@ import pygame
 import random
 
 # Constants
-POPULATION_SIZE = 50
+POPULATION_SIZE = 100
 MUTATION_RATE = 0.1
-TARGET_RECT = pygame.Rect(400, 300, 100, 100)
+TARGET_RECT = pygame.Rect(300, 200, 200, 200)
 SCREEN_SIZE = (800, 600)
-RECT_SIZE = (20, 20)
-BG_COLOR = (255, 255, 255)
-RECT_COLOR = (0, 0, 0)
+RECT_SIZE = (15, 15)
+BG_COLOR = (0,0,0)
+RECT_COLOR = (255, 255, 255)
 
 # Initialize Pygame
 pygame.init()
 screen = pygame.display.set_mode(SCREEN_SIZE)
+
+def random_color():
+    return (random.randint(0,255),random.randint(0,255),random.randint(0,255))
 
 # Define the Rectangle class
 class Rectangle:
@@ -20,13 +23,13 @@ class Rectangle:
         if rect:
             self.rect = rect
         else:
-            self.rect = pygame.Rect(random.randint(0, SCREEN_SIZE[0] - RECT_SIZE[0]), 
-                                    random.randint(0, SCREEN_SIZE[1] - RECT_SIZE[1]), 
+            self.rect = pygame.Rect(random.randint(0, SCREEN_SIZE[0]), 
+                                    random.randint(0, SCREEN_SIZE[1]), 
                                     RECT_SIZE[0], RECT_SIZE[1])
         self.fitness = None
         
     def draw(self):
-        pygame.draw.rect(screen, RECT_COLOR, self.rect)
+        pygame.draw.rect(screen, random_color(), self.rect)
         
     def calculate_fitness(self):
         distance = pygame.math.Vector2(TARGET_RECT.center) - pygame.math.Vector2(self.rect.center)
@@ -34,8 +37,8 @@ class Rectangle:
         
     def mutate(self):
         if random.random() < MUTATION_RATE:
-            self.rect.x += random.randint(-10, 10)
-            self.rect.y += random.randint(-10, 10)
+            self.rect.x += random.randint(-1, 1)
+            self.rect.y += random.randint(-1, 1)
 
 # Define the Population class
 class Population:
@@ -71,13 +74,15 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN:
             
-    screen.fill(BG_COLOR)
-    
-    population.draw()
-    population.calculate_fitnesses()
-    population.reproduce()
-    
-    pygame.draw.rect(screen, RECT_COLOR, TARGET_RECT, 3)
-    
-    pygame.display.update()
+                screen.fill(BG_COLOR)
+
+                population.draw()
+                population.calculate_fitnesses()
+                population.reproduce()
+
+                pygame.draw.rect(screen, RECT_COLOR, TARGET_RECT, 3)
+
+                pygame.display.update()
